b8b22bb7347543d7c2f63b9340668c096738761b
AlexEaton1105 <alexeaton11@hotmail.co.uk>
2015-09-16 11:38:47 +0100

    Create Python Bronze Tasks.py

---
+++ Python Bronze Tasks.py
@@ -0,0 +1,6 @@
+print("Welcome to python")
+print(10+5)
+#created by alex eaton 14.09.2015
+#comments are useful when using code as they help you learn and understand whats happening.
+print("Hello how are you?\nI hope you are well today.")
+      
