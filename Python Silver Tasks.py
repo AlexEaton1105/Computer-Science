35c02326ed9800a69cb1d3a5adb980784475c35b
AlexEaton1105 <alexeaton11@hotmail.co.uk>
2015-09-16 11:39:31 +0100

    Create Python Silver Tasks.py

---
+++ Python Silver Tasks.py
@@ -0,0 +1,16 @@
+number1 = 5
+print(number1)
+#a variable is somethig that can store another value
+number1 = 10
+number2 = 5
+print(number1+number2)
+
+qone = int(input("choose first number "))
+qtwo = int(input("choose second number "))
+print(qone+qtwo)
+
+qthree = input("What is your name? ")
+print("Welcome to python, " , qthree)
+
+
+
