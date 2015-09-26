b2f7ee1847555e056bfc9739b90ede73b1c8ca5a
A1ex <alexeaton11@hotmail.co.uk>
2015-09-26 16:55:07 +0100

    Update arithmetic fail.py

--- arithmetic fail.py
+++ arithmetic fail.py
@@ -48,9 +48,9 @@ print("That's it! the quiz is over. Your overall score was: ",score,"Well done!"
 #now you need to make it so that if they got less than 5, you tell them to improve,  less than 7 they could try harder and more they have done very well.
 #do this by replacing this comment with an if, elif and else
 
-if score >= 7:
-    print("That's it! the quiz is over. Your overall score was: ",score,"Well done!")
+if score <= 4:
+    print("That's it! the quiz is over. Your overall score was: ",score,"Better luck next time!!")
 elif score <= 7:
     print("That's it! the quiz is over. Your overall score was: ",score," Good try!")
 else:
-    print ("That's it! the quiz is over. Your overall score was: ",score," Better luck next time!")
+    print ("That's it! the quiz is over. Your overall score was: ",score," Well done!")
