069bf8bef531780a383230af2a75a5ef8c26ced2
A1ex <alexeaton11@hotmail.co.uk>
2015-10-07 11:13:11 +0100

    Update and rename arithmetic fail.py to arithmetic complete.py

--- arithmetic complete.py
+++ arithmetic complete.py
@@ -25,23 +25,23 @@ for counter in range(0,10):#ths makes sure there is only 10 questions asked.
     numberTwo = random.randint(1,10)
     operator = random.randint(1,3)
 
-    if operator == 1: #this changes whether the question is an add, subtract or multiply.
-      print("Question ",counter,"What is ",numberOne, "+", numberTwo)
-      ans = add(numberOne, numberTwo)
-    elif operator == 2:
-      print("Question ",counter,"What is ",numberOne, "-",numberTwo)
-      ans = subtract(numberOne, numberTwo)
-    else:
-      print("Question ",counter,"What is ",numberOne, "*",numberTwo)
-      ans = multiply(numberOne, numberTwo)
+  if operator == 1: #this changes whether the question is an add, subtract or multiply.
+    print("Question ",counter,"What is ",numberOne, "+", numberTwo)
+    ans = add(numberOne, numberTwo)
+  elif operator == 2:
+    print("Question ",counter,"What is ",numberOne, "-",numberTwo)
+     ans = subtract(numberOne, numberTwo)
+  else:
+    print("Question ",counter,"What is ",numberOne, "*",numberTwo)
+    ans = multiply(numberOne, numberTwo)
 
-    res = int(input("Please write answer here: ")) #this is where the user enters their answer.
-    if res == ans:
-      print("Well done, ",name,"! 1 point scored.")
-      time.sleep(1)
-      score = score + 1
-    else:
-      print("Unlucky, ",name,". No points scored.")
+      res = int(input("Please write answer here: ")) #this is where the user enters their answer.
+  if res == ans:
+    print("Well done, ",name,"! 1 point scored.")
+    time.sleep(1)
+    score = score + 1
+  else:
+    print("Unlucky, ",name,". No points scored.")
 
 #make it so that if they got less than 4, you tell them to improve,  less than 7 they could try harder and more they have done very well.
 #do this by replacing this comment with an if, elif and else
